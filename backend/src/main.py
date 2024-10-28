07d34ffcf3fffbbcc22c2b07372dfb7bbfe56cb2
simon-builder <simonma.devs@gmail.com>
2024-10-28 21:38:43 +0100

    added working create_todo_list route

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,22 +3,22 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.server_api import ServerApi
 
 from src.api.main import api_router
-from .database import connect_db, close_db, get_collection
+from .database import get_db, close_db, get_collection
 from .config import settings
 
-async def lifespan(app: FastAPI):
-    await connect_db()
-    todo_lists = get_collection(settings.collection)
-    # app.todo_dal = ToDoDAL(todo_lists)
+# async def lifespan(app: FastAPI):
+#     await connect_db()
+#     global todo_lists
+#     todo_lists = get_collection(settings.collection)
+#     # app.todo_dal = ToDoDAL(todo_lists)
     
-    # Yield back to FastAPI Application:
-    yield
-    await close_db()
+#     # Yield back to FastAPI Application:
+#     yield
+#     await close_db()
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
-# app.include_router(api_router, )
 
 @app.get("/")
 async def root():
